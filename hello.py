a2b8748c6644d798a8f4276a84941618dfafadbb
meixir913 <62222967+meixir913@users.noreply.github.com>
2020-03-18 12:59:12 -0400

    Hello py

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("hello world")
+#print(hellp world)--NameError, not defined
+#what is value?: hello world
+#what is operator? (mathematical operator: +,* with priorities)
+name = input('Please enter your name: ') #define the variable
+print('Hello,', name)
+#input()--function
+grade="A" 
+#Right side is expression e.g. "A",input('Please enter your name: ')
+#Expression--NO "="
+#Statement--WITH "=" e.g. grade="A"
+print(name,"got a grade", grade)
